6d2eb7447733f80a73048145c52e2ee2fdf73e57
buz galbraith <buzgalbraith@gmail.com>
2025-12-17 11:52:44 -0500

    cleaned up sbatch script

--- scripts/sbatch_run.sh
+++ scripts/sbatch_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH -J Assemble_NF_KG                            # Job name
-#SBATCH -N 1                                # Number of nodes
-#SBATCH -n 16                               # Number of tasks
-#SBATCH -o ./outputs/output_%j.txt                    # Standard output file
-#SBATCH -e ./outputs/error_%j.txt                     # Standard error file
-#SBATCH --mail-user=$USER@northeastern.edu  # Email
+#SBATCH -N 2                                # Number of nodes
+#SBATCH -n 32                               # Number of tasks
+#SBATCH -o ./outputs/%x-%j.out
+#SBATCH -e ./outputs/%x-%j.err
+#SBATCH --mail-user=w.galbraith@northeastern.edu  # Email
 #SBATCH --mail-type=ALL                     # Type of email notifications
-#SBATCH --time=12:00:00
-$gl/dglink/.venv/bin/python3 ./scripts/assemble_nf_kg.py
+#SBATCH --time=1-12:00:00 # 1 day 12 hours zero minutes
+source $gl/dglink/.venv/bin/activate
+python ./scripts/assemble_nf_kg.py
